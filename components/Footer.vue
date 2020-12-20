30303d1fed47b1d1c6922292025f0638618f0117
geekmaros <razicruz@gmail.com>
2020-12-20 17:38:28 +0100

    added footer mobile view

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    Design inspiration by Brittany Chiang. Built with code and ☕ by
-    <a class="text-regal-pink" href="">geekmaros</a>
+  <div class=" text-white text-center pb-8 font-sans text-sm px-8 md:px-0">
+    Design inspiration by
+    <a
+      class="text-regal-blue hover:text-regal-pink font-semibold"
+      href="https://brittanychiang.com/"
+      >Brittany Chiang</a
+    >. Built with 💻 and ☕ by
+    <a
+      class="text-regal-pink hover:text-regal-blue font-semibold"
+      href="https://github.com/geekmaros"
+      >geekmaros</a
+    >
   </div>
 </template>
 
