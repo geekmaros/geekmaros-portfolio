472ed5f008292d57732f27e84cb06d078bdb90b9
geekmaros <razicruz@gmail.com>
2020-12-26 17:48:15 +0100

    finishing touches

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,13 +34,13 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['@/plugins/aos.client'],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxt/components',
